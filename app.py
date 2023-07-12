7cf8eda9656b7617cfc83cee3dbfc8d6396952d4
marius-george <>
2023-07-12 09:37:14 +0000

    Create base template

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from pymongo import MongoClient
+
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 
 @app.route("/")
+@app.route("/get_tasks")
+def get_tasks():
+    mongo_uri = app.config["MONGO_URI"]
+    database_name = "task_manager"
 
-def hello():
-    return "Hello world.. again!"
+    client = MongoClient(mongo_uri)
+    db = client[database_name]
+    tasks = db.tasks.find()
+
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(host=os.environ.get("IP"), port=int(
+        os.environ.get("PORT")), debug=True)
