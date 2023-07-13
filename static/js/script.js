dedc761d047929c0658dd3e2a0d2606ac4cff0b1
marius-george <>
2023-07-13 13:32:03 +0000

    Add Navbar and Sidebar

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+/*
+    jQuery for MaterializeCSS initialization
+*/
+
+$(document).ready(function () {
+  $(".sidenav").sidenav({ edge: "right" });
+});
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+// });
